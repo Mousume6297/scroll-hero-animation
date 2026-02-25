e042f9449c2703b0ad681d71882624f11807d4a2
Mousume Barick <mousumebarick@gmail.com>
2026-02-25 15:56:27 +0530

    done js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-// ===== Intro Animation =====
+
 gsap.to(".headline", {
   opacity: 1,
   y: -15,
@@ -15,23 +15,23 @@ gsap.to(".stat", {
   ease: "power3.out"
 });
 
-// ===== Smooth Scroll Animation =====
+
 const car = document.querySelector(".car");
 
 let current = 0;
 let target = 0;
 
-// smooth interpolation
+
 function animate() {
 
   target = window.scrollY;
   current += (target - current) * 0.08;
 
-  // smoother values (fixed)
+ 
   const translateY = current * 0.35;
   const scale = 1 + current * 0.0002;
 
-  // removed heavy rotation (causing issue)
+
   car.style.transform =
     `translateY(${translateY}px) scale(${scale})`;
 
